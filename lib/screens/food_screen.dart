b3257a0b1b2809e7fc6e9b9cd4685458159c01ce
nischal08 <nischal.karanjit@gmail.com>
2021-06-17 22:13:26 +0545

    added styling to food page grid

--- lib/screens/food_screen.dart
+++ lib/screens/food_screen.dart
@@ -1,43 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:tomato_app/contants/color_properties.dart';
 import 'package:tomato_app/controller/food_controller.dart';
 
 class FoodScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    FoodController _foodStateController = Provider.of<FoodController>(context);
+    FoodController _foodStateController = Provider.of<FoodController>(
+      context,
+      listen: false,
+    );
     return Container(
-      child: FutureBuilder(
-        future: _foodStateController.getFoodCategory(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-          return GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 1,
-              mainAxisSpacing: 1,
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          SafeArea(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.60,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Food at your service ",
+                    textAlign: TextAlign.start,
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "Any food you like ",
+                    textAlign: TextAlign.start,
+                    style: Theme.of(context).textTheme.headline6!.copyWith(
+                          color: Theme.of(context).accentColor,
+                        ),
+                  ),
+                ],
+              ),
             ),
-            itemCount: _foodStateController.categoryItems.length,
-            itemBuilder: (context, index) {
-              return GridTile(
-                footer: Container(
-                  padding: EdgeInsets.symmetric(vertical: 5),
-                  color: Theme.of(context).accentColor.withOpacity(0.1),
-                  child: Text(_foodStateController.categoryItems[index].name,
-                      textAlign: TextAlign.center),
-                ),
-                child: ClipRRect(
-                  child: Image.network(
-                      _foodStateController.categoryItems[index].image),
-                ),
-              );
-            },
-          );
-        },
+          ),
+          Expanded(
+            child: Container(
+              constraints: BoxConstraints(
+                minWidth: MediaQuery.of(context).size.width * 0.45,
+                maxWidth: MediaQuery.of(context).size.width * 0.90,
+              ),
+              child: FutureBuilder(
+                future: _foodStateController.getFoodCategory(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  return GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 3 / 2,
+                      crossAxisSpacing: 15,
+                      mainAxisSpacing: 30,
+                    ),
+                    itemCount: _foodStateController.categoryItems.length,
+                    itemBuilder: (context, index) {
+                      return GridTile(
+                        footer: Container(
+                          decoration: BoxDecoration(
+                            color:
+                                Theme.of(context).primaryColor.withOpacity(0.4),
+                          ),
+                          padding: EdgeInsets.symmetric(vertical: 5),
+                          child: Text(
+                            _foodStateController.categoryItems[index].name,
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline6!
+                                .copyWith(color: Colors.white),
+                          ),
+                        ),
+                        child: Container(
+                          child: Image.network(
+                            _foodStateController.categoryItems[index].image,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
